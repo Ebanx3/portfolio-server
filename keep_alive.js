5998c9e24f56fb5bd2e2f3013cc8ee735c3688cd
Ebanx3 <esteban.dos.santos@gmail.com>
2024-10-22 22:53:21 -0300

    added keep_alive file

--- keep_alive.js
+++ keep_alive.js
@@ -1,8 +1,10 @@
 import http from "http";
 
-http
-  .createServer((req, res) => {
-    res.write("I am alive!");
-    res.end();
-  })
-  .listen(8080);
+export const keep_alive = () => {
+  http
+    .createServer((req, res) => {
+      res.write("I am alive!");
+      res.end();
+    })
+    .listen(8080);
+};
