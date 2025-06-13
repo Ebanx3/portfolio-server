9ea53d91ddba3ff419ddd7d6ca51310b4406ede9
Ebanx3 <esteban.dos.santos@gmail.com>
2025-06-12 23:44:43 -0300

    little correction to import

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "url";
 import { addFrontendUrlEmail, addNewVisit, getEmail } from "./redis.js";
-import { sendEmail } from "./sendEMail.js";
+import { sendEmail } from "./sendEmail.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
